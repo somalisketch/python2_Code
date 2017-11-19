532dbc15357dbc7a18feb937d000c723e49740e8
Nasser Salah <33800887+somalisketch@users.noreply.github.com>
2017-11-19 15:15:52 +0300

    first code to post

---
+++ input_output.py
@@ -0,0 +1,8 @@
+print("hello there")
+name = input("what's your name ")
+print("hello " + name) # adding name to string
+print("what is your favorite number ")
+number = input("Type it Here ")
+intnumber = int(number) * 3
+
+print("favorite number is :" + str(intnumber))
