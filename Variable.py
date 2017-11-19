35365d1fce0001845d3b1990f45ea6a2f28237dd
Nasser Salah <33800887+somalisketch@users.noreply.github.com>
2017-11-19 15:13:05 +0300

    first code to post

---
+++ Variable.py
@@ -0,0 +1,10 @@
+mystring = "hello world"
+myint = 13
+myfloat = 13.3
+
+print(type(mystring))
+print(type(myint))
+print(type(myfloat))
+
+myint = "heyyyy" #converting type
+print("changed type " + myint, type(myint))
